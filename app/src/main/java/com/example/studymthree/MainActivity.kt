67a7da5f7620339d09f5f52fc2648eb36d343129
aboudmahmoud <aboudmahmoud40@gmail.com>
2022-10-03 11:49:17 +0400

    Edit Text

--- app/src/main/java/com/example/studymthree/MainActivity.kt
+++ app/src/main/java/com/example/studymthree/MainActivity.kt
@@ -8,28 +8,25 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.selection.DisableSelection
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material3.*
 
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.BaselineShift
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+
 import com.example.studymthree.ui.theme.StudymthreeTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,7 +35,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             StudymthreeTheme {
                 // A surface container using the 'background' color from the theme
-                ExtandCostemcard()
+                Greeting()
             }
         }
     }
@@ -47,69 +44,50 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting() {
-    ColumnTexts()
-}
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        var Text by remember {
+            mutableStateOf("")
+        }
+        TextField(
+            value = Text,
+            placeholder = {
+                Text(text = "Enatr ur email")
+            },
+            onValueChange = { newText ->
+                Text = newText
 
-@Composable
-fun ColumnTexts() {
-    Column() {
-        SuperScripetText("1","2")
-        SubScripetText("2","1")
+            },
+            label = {
+                Text("Label")
+            },
+            leadingIcon = {
+                IconButton(onClick = {}) {
+                    Icon(imageVector = Icons.Filled.Email, contentDescription = "")
+                }
+            },
+            trailingIcon = {
+                IconButton(onClick = {}) {
+                    Icon(imageVector = Icons.Filled.Check, contentDescription = "")
+                }
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Go
+            ),
+        )
     }
 }
-@Composable
-fun SuperScripetText(
-    normalText: String,
-    superText: String,
-) {
-    Text(buildAnnotatedString {
-        withStyle(
-            style = SpanStyle(
-                fontSize = MaterialTheme.typography.labelMedium.fontSize
-            )
-        ) {
-            append(normalText)
-        }
 
-        withStyle( style = SpanStyle(
-            fontSize = MaterialTheme.typography.labelSmall.fontSize,
-            fontWeight = FontWeight.Normal,
-            baselineShift = BaselineShift.Superscript
-        )
-        ){
-            append(superText)
-        }
-    })
-}
-@Composable
-fun SubScripetText(
-    normalText: String,
-    SubText: String,
-) {
-    Text(buildAnnotatedString {
-        withStyle(
-            style = SpanStyle(
-                fontSize = MaterialTheme.typography.labelMedium.fontSize
-            )
-        ) {
-            append(normalText)
-        }
 
-        withStyle( style = SpanStyle(
-            fontSize = MaterialTheme.typography.labelSmall.fontSize,
-            fontWeight = FontWeight.Normal,
-            baselineShift = BaselineShift.Subscript
-        )
-        ){
-            append(SubText)
-        }
-    })
-}
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     StudymthreeTheme {
-        ExtandCostemcard()
+        Greeting()
 
     }
 }
