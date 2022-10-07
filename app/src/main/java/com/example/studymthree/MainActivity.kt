08c430fd9d912a726a149894ceee8d695dc8bdd9
aboudmahmoud <aboudmahmoud40@gmail.com>
2022-10-07 12:53:41 +0400

    Password Text Field

--- app/src/main/java/com/example/studymthree/MainActivity.kt
+++ app/src/main/java/com/example/studymthree/MainActivity.kt
@@ -18,7 +18,10 @@ import androidx.compose.material.icons.filled.Email
 import androidx.compose.material3.*
 
 import androidx.compose.runtime.Composable
+
+
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -27,6 +30,8 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -50,6 +55,7 @@ class MainActivity : ComponentActivity() {
 }
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Greeting() {
     Column(
@@ -57,50 +63,36 @@ fun Greeting() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        CoilImage()
-        AsyncImage(
-            model = ImageRequest.Builder(LocalContext.current)
-                .data("https://gccgamers.com/media/magefan_blog/gaming-pc.jpg")
-                .crossfade(true)
-                .build(),
-            placeholder = painterResource(R.drawable.wait),
-            error = painterResource(R.drawable.error) ,
-            contentDescription = "s",
-            contentScale = ContentScale.Crop,
-            modifier = Modifier.clip(CircleShape)
+        var password by rememberSaveable {
+            mutableStateOf("")
+        }
+        var PaasswordStates by remember{
+            mutableStateOf(false)
+        }
+
+        val icones=if(PaasswordStates)painterResource(id = R.drawable.passwordvispel)
+        else painterResource(id = R.drawable.passworddisaple)
+
+        OutlinedTextField(value = password, onValueChange = {
+            password = it
+        },
+            placeholder = { Text(text = "Password") },
+            label = { Text("Password") },
+            trailingIcon = {
+                IconButton(onClick = { PaasswordStates=!PaasswordStates}) {
+                    Icon(
+                        painter = icones,
+                        contentDescription = "password visible",
+
+                    )
+                }
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType =  KeyboardType.NumberPassword
+            ),
+            visualTransformation = if(PaasswordStates) VisualTransformation.None
+        else PasswordVisualTransformation()
         )
-    }
-}
-
-@Composable
-fun CoilImage() {
-    Box(
-        modifier = Modifier
-            .width(150.dp)
-            .height(150.dp),
-        contentAlignment = Alignment.Center
-    ) {
-        val painter = rememberAsyncImagePainter(ImageRequest.Builder(LocalContext.current)
-            .data("https://gccgamers.com/media/magefan_blog/gaming-pc.jpg").apply(
-            {
-                placeholder(R.drawable.wait)
-                error(R.drawable.error)
-                crossfade(1000)
-               transformations(
-                   CircleCropTransformation(),
-
-               )
-
-            }).build()
-        )
-        Image(
-            painter = painter,
-            contentDescription = "logo image",
-            modifier = Modifier.fillMaxSize()
-        )
-
-
-
     }
 }
 
